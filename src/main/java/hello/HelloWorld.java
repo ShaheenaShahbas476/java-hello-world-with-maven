ad0c27e26b5a60ecfc869bda21efa729d764cc98
Shaheena <shaheenashahbas476@gmail.com>
2025-11-02 18:31:04 +0530

    Updated HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,9 +5,10 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
+		  System.out.println("Hi ! Im Shaheena! The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
+        System.out.println("Hi ! Im Shaheena! The current local time is: " + currentTime);
         System.out.println(greeter.sayHello());
     }
 }
